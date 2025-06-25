217bfd98e68b1f7269b40af4ed913960a5f22014
Mehrshad <ytmehrshad@gmail.com>
2025-06-25 11:00:04 +0330

    added all the changes from bale bot to telegram bot

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import json
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
-from functions import subjects_keyboard_creator, topics_keyboard_creator, forward_file
+from functions import subjects_keyboard_creator, pamphlets_keyboard_creator, refrences_keyboard_creator, content_keyboard_creator, forward_file
 
 import os
 
@@ -11,7 +11,7 @@ with open("./files_data.json", "r", encoding="utf-8") as files_data_json:
     files_data = json.load(files_data_json)
 
 CHANNEL_ID = -1002468726141
-CHANNEL_USERNAME = "@Medicology_ABZUMS"
+CHANNEL_USERNAME = "@medicology_abzums"
 
 
 async def is_user_member(channel_username: str, user_id: int, context):
@@ -43,15 +43,16 @@ async def start(update: Update, context=ContextTypes.DEFAULT_TYPE):
         )
         return
 
-    reply_markup = subjects_keyboard_creator(files_data)
+    reply_markup = content_keyboard_creator(contents=files_data)
 
     if update.message:
-        await update.message.reply_text("برای چه درسی جزوه میخوای؟", reply_markup=reply_markup)
+        await update.message.reply_text("جزوه میخوای یا رفرنس؟", reply_markup=reply_markup)
 
     if update.callback_query:
         query = update.callback_query
-        await query.answer()
-        await query.edit_message_text("برای چه درسی جزوه میخوای؟", reply_markup=reply_markup)
+        # await query.answer()
+        await context.bot.edit_message_text(text="جزوه میخوای یا رفرنس؟", message_id=query.message.message_id, chat_id=chat_id, reply_markup=reply_markup)
+        # await query.edit_message_text("برای چه درسی جزوه میخوای؟", reply_markup=reply_markup)
 
 
 async def handle_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -81,36 +82,68 @@ async def handle_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if query.data == 'start':
         await start(update=update, context=context)
 
-    if query.data.split('_')[1] == '-1':
+    if query.data.split('_')[1] == '-1' or query.data.split('_')[1] == '[]':
         await context.bot.send_message(
             chat_id=chat_id,
-            text="متاسفانه جزوه این مبحث هنوز داخل ربات قرار نگرفته ولی به زودی قرار میگیره ❤\n\nاگر درس یا مبحثی کلا توی ربات نیست و به جزوه اش نیاز داری یا اینکه جزوه خوبی داری که میخوای به اشتراک بذاری که داخل ربات قرار بگیره، به من پیام بده:\n@MehrshadChKh",
+            text="متاسفانه جزوه یا رفرنس این مبحث هنوز داخل ربات قرار نگرفته\n\nاگر درس یا مبحثی کلا توی ربات نیست و به جزوه یا رفرنس اش نیاز داری یا اینکه جزوه خوبی داری یا رفرنسی داری که داخل ربات نیست و میخوای به اشتراک بذاری که داخل ربات قرار بگیره، به من پیام بده ❤\n@MehrshadChKh",
         )
         return
 
     if query.data.split('_')[0] == "back":
-        if query.data.split('_')[1] == "subjects":
-            reply_markup = subjects_keyboard_creator(files_data)
-            await query.edit_message_text("برای چه درسی جزوه میخوای؟", reply_markup=reply_markup)
+        if query.data.split('_')[1] == "contents":
+            reply_markup = content_keyboard_creator(files_data)
+            await context.bot.edit_message_text(text="جزوه میخوای یا رفرنس؟", message_id=query.message.message_id, chat_id=chat_id, reply_markup=reply_markup)
             return
 
-    if query.data.split('_')[0] == "topics":
-        reply_markup = topics_keyboard_creator(query.data.split('_')[1])
-        await query.edit_message_text(f'درس: {files_data[query.data.split("_")[1]]["name"]}\n\nخب حالا مبحث مورد نظرتو انتخاب کن:', reply_markup=reply_markup)
+        if query.data.split('_')[1] == "pamphlets":
+            print(query.data.split('_')[1])
+            reply_markup = subjects_keyboard_creator(
+                files_data["pamphlets"]["subjects"], content="pamphlets")
+            await context.bot.edit_message_text(text="نوع محتوا: جزوه \n\n برای چه درسی جزوه میخوای؟", message_id=query.message.message_id, chat_id=chat_id, reply_markup=reply_markup)
+            # await query.edit_message_text("برای چه درسی جزوه میخوای؟", reply_markup=reply_markup)
+            return
+
+        if query.data.split('_')[1] == "refrences":
+            reply_markup = subjects_keyboard_creator(
+                files_data["refrences"]["subjects"], content="refrences")
+            await context.bot.edit_message_text(text="نوع محتوا: رفرنس \n\n رفرنس چه درسی رو میخوای؟", message_id=query.message.message_id, chat_id=chat_id, reply_markup=reply_markup)
+            # await query.edit_message_text("برای چه درسی جزوه میخوای؟", reply_markup=reply_markup)
+            return
+
+    if query.data.split('_')[0] == "contents":
+        if query.data.split('_')[1] == "pamphlets":
+            reply_markup = subjects_keyboard_creator(
+                files_data["pamphlets"]["subjects"], content="pamphlets")
+            await context.bot.edit_message_text(text="نوع محتوا: جزوه \n\n برای چه درسی جزوه میخوای؟", message_id=query.message.message_id, chat_id=chat_id, reply_markup=reply_markup)
+
+        if query.data.split('_')[1] == "refrences":
+            reply_markup = subjects_keyboard_creator(
+                files_data["refrences"]["subjects"], content="refrences")
+            await context.bot.edit_message_text(text="نوع محتوا: رفرنس \n\n رفرنس چه درسی رو میخوای؟", message_id=query.message.message_id, chat_id=chat_id, reply_markup=reply_markup)
+
+    if query.data.split('_')[0] == "pamphlets":
+        reply_markup = pamphlets_keyboard_creator(query.data.split('_')[1])
+        await context.bot.edit_message_text(text=f'نوع محتوا: جزوه\nدرس: {files_data["pamphlets"]["subjects"][query.data.split("_")[1]]["name"]}\n\nخب حالا مبحث مورد نظرتو انتخاب کن:', message_id=query.message.message_id, chat_id=chat_id, reply_markup=reply_markup)
+        # await query.edit_message_text(text=f'درس: {files_data[query.data.split("_")[1]]["name"]}\n\nخب حالا مبحث مورد نظرتو انتخاب کن:', reply_markup=reply_markup)
+        return
+
+    if query.data.split('_')[0] == "refrences":
+        reply_markup = refrences_keyboard_creator(query.data.split('_')[1])
+        await context.bot.edit_message_text(text=f'نوع محتوا: رفرنس\nرفرنس: {files_data["refrences"]["subjects"][query.data.split("_")[1]]["name"]}\n\nانگلیسی یا ترجمه شده؟', message_id=query.message.message_id, chat_id=chat_id, reply_markup=reply_markup)
+        # await query.edit_message_text(text=f'درس: {files_data[query.data.split("_")[1]]["name"]}\n\nخب حالا مبحث مورد نظرتو انتخاب کن:', reply_markup=reply_markup)
         return
 
     if query.data.split('_')[0] == "send-file":
-        file_id = int(query.data.split('_')[1])
+        file_id = query.data.split('_')[1]
         chat_id = query.message.chat.id
 
         await forward_file(file_id=file_id, chat_id=chat_id,
-                           channel_username=CHANNEL_USERNAME, context=context)
+                           channel_id=CHANNEL_ID, context=context)
         return
 
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+app = ApplicationBuilder().token(BOT_TOKEN).base_url().build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(handle_buttons))
 
 app.run_polling()
-print("Bot started...")
--- functions.py
+++ functions.py
@@ -1,16 +1,28 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 import json
+import ast
 
 with open("./files_data.json", "r", encoding="utf-8") as files_data_json:
     files_data = json.load(files_data_json)
 
 
-def subjects_keyboard_creator(subjects_data):
+def add_back_button(keyboard, back_to_where):
+    if len(keyboard[-1]) == 1:
+        keyboard[-1] = [InlineKeyboardButton(
+            "منوی قبلی ⬅", callback_data=f"back_{back_to_where}"), keyboard[-1][0],]
+    elif len(keyboard[-1]) == 2:
+        keyboard.append([InlineKeyboardButton(
+            "منوی قبلی ⬅", callback_data=f"back_{back_to_where}")])
+
+    return keyboard
+
+
+def content_keyboard_creator(contents):
     keyboard_one_row = [
-        InlineKeyboardButton(subjects_data[subject]["name"],
-                             callback_data=f'topics_{subjects_data[subject]["callback_data"]}')
-        for subject in subjects_data
+        InlineKeyboardButton(contents[content]["name"],
+                             callback_data=f'contents_{contents[content]["callback_data"]}')
+        for content in contents
     ]
 
     keyboard_two_rows = [keyboard_one_row[i:i+2]
@@ -20,8 +32,24 @@ def subjects_keyboard_creator(subjects_data):
     return reply_markup
 
 
-def topics_keyboard_creator(subject_data):
-    topics = files_data[subject_data]["topics"]
+def subjects_keyboard_creator(subjects_data, content):
+    keyboard_one_row = [
+        InlineKeyboardButton(text=subjects_data[subject]["name"],
+                             callback_data=f'send-file_{subjects_data[subject]["files"]}' if "files" in subjects_data[subject] else f'{content}_{subjects_data[subject]["callback_data"]}')
+        for subject in subjects_data
+    ]
+
+    keyboard_two_rows = [keyboard_one_row[i:i+2]
+                         for i in range(0, len(keyboard_one_row), 2)]
+
+    final_keyboard = add_back_button(
+        keyboard=keyboard_two_rows, back_to_where="contents")
+    reply_markup = InlineKeyboardMarkup(final_keyboard)
+    return reply_markup
+
+
+def pamphlets_keyboard_creator(subject_data):
+    topics = files_data["pamphlets"]["subjects"][subject_data]["topics"]
 
     keyboard_one_row = [
         InlineKeyboardButton(topics[topic]["name"], callback_data=f'send-file_{topics[topic]["file_id"]}') for topic in topics
@@ -30,22 +58,46 @@ def topics_keyboard_creator(subject_data):
     keyboard_two_rows = [keyboard_one_row[i:i+2]
                          for i in range(0, len(keyboard_one_row), 2)]
 
-    if len(keyboard_two_rows[-1]) == 1:
-        keyboard_two_rows[-1] = [InlineKeyboardButton(
-            "منوی قبلی ⬅", callback_data='back_subjects'), keyboard_two_rows[-1][0],]
-    elif len(keyboard_two_rows[-1]) == 2:
-        keyboard_two_rows.append([InlineKeyboardButton(
-            "منوی قبلی ⬅", callback_data='back_subjects')])
+    final_keyboard = add_back_button(
+        keyboard=keyboard_two_rows, back_to_where="pamphlets")
 
-    reply_markup = InlineKeyboardMarkup(keyboard_two_rows)
+    reply_markup = InlineKeyboardMarkup(final_keyboard)
     return reply_markup
 
 
-async def forward_file(chat_id, channel_username, file_id, context):
-    await context.bot.forward_message(
-        chat_id=chat_id, from_chat_id=channel_username, message_id=file_id)
+def refrences_keyboard_creator(subject_data):
+    refrences = files_data["refrences"]["subjects"][subject_data]
+
+    keyboard_one_row = [
+        InlineKeyboardButton(
+            "انگلیسی", callback_data=f'send-file_{refrences["english"]["files"]}'),
+        InlineKeyboardButton(
+            "ترجمه", callback_data=f'send-file_{refrences["translated"]["files"]}')
+    ]
+
+    keyboard_two_rows = [keyboard_one_row[i:i+2]
+                         for i in range(0, len(keyboard_one_row), 2)]
+
+    final_keyboard = add_back_button(
+        keyboard=keyboard_two_rows, back_to_where="refrences")
+
+    reply_markup = InlineKeyboardMarkup(final_keyboard)
+    return reply_markup
+
+
+async def forward_file(chat_id, channel_id, file_id, context):
+    file_id_evaluated = ast.literal_eval(file_id)
+
+    if isinstance(file_id_evaluated, int):
+        await context.bot.forward_message(
+            chat_id=chat_id, from_chat_id=channel_id, message_id=file_id_evaluated)
+
+    if isinstance(file_id_evaluated, list):
+        for file in file_id_evaluated:
+            await context.bot.forward_message(
+                chat_id=chat_id, from_chat_id=channel_id, message_id=file)
 
     keyboard = [[InlineKeyboardButton('استارت', callback_data='start')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await context.bot.send_message(chat_id=chat_id, text="بفرما اینم از جزوه مد نظرت ✌\n\nاگر جزوه دیگه ای مد نظرته، دوباره ربات رو استارت کن ❤", reply_markup=reply_markup)
+    await context.bot.send_message(chat_id=chat_id, text="بفرما اینم از جزوه یا رفرنس مد نظرت ✌\n\nاگر جزوه یا رفرنس دیگه ای مد نظرته، دوباره ربات رو استارت کن ❤", reply_markup=reply_markup)
